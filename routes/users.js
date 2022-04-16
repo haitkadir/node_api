e237a35f51392b442d61624fa73dcf9868b64e59
Hssain Aitkadir <haitkadi@e2r13p3.1337.ma>
2022-04-16 21:58:54 +0000

    An api using node and express js

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { v4 as uuidv4 } from 'uuid';
+
+
+const router = express.Router();
+
+const users = []
+
+router.get('/', (req, res) => {
+    res.send(users);
+});
+
+
+router.post('/', (req, res) => {
+
+    const user = req.body;
+
+    users.push({ ... user, id: uuidv4() });
+    res.send(`User with a name:${user.firstName} added to database`);
+});
+
+export default router;
